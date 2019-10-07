395131414979a7cd7389addb04a467f355bdccd3
MORTREUX Florian <mortflo@ndlp.fr>
2019-10-07 11:38:21 +0200

    [CORE] Category entity

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,15 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
-    /**
-     * @Route("/", name="default")
-     */
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("index", name="index")
+     * @Route("/", name="index")
      */
     public function index()
     {
@@ -21,4 +18,21 @@ class DefaultController extends AbstractController
         ]);
     }
 
+    /**
+    * @Route("/user", name="index_user")
+    */
+    public function user()
+{
+    $usersList = array();
+
+    $usersList[0]['first_name'] = 'Crystal';
+    $usersList[0]['last_name'] = 'E. Everns';
+
+    $usersList[1]['first_name'] = 'Erevans';
+    $usersList[1]['last_name'] = 'Leonherd';
+    
+        return $this->render('user/user.html.twig', [
+            'users_list' => $usersList,
+        ]);
+}
 }
