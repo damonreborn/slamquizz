19b4681cc6e6ab5d72e7890647497952cb012139
MORTREUX Florian <mortflo@ndlp.fr>
2019-11-25 11:15:33 +0100

    [CORE] Test Unitaire Ajouter

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -3,6 +3,9 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class SecurityControllerTest extends WebTestCase
 {
@@ -19,41 +22,36 @@ class SecurityControllerTest extends WebTestCase
     public function testShowLogin()
     {
         // Request /login 
-        $this->client->request('GET', '/login');
+        //$this->client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
 
         // Asserts that /login path exists and don't return an error
-        /* 
-        Ecrire ici le code pour valider le succès de la réponse HTTP,
-        c'est à dire affirmer que le code de statut de la réponse est égale à 200 (Response::HTTP_OK)
-        */
-       
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
         
         // Asserts that the phrase "Log in!" is present in the page's title
-        /* 
-        Ecrire ici le code pour vérifier la présence de la phrase "Log in!" dans le titre de la page,
-        c'est à dire affirmer qu'en parcourant le DOM, la balise 'html head title' contient 'Log in!'
-        */ 
+        
+        $this->assertSelectorTextContains('html head title', 'Log in!');
 
 
         // Asserts that the response content contains 'csrf token'
-        /*
-        Ecrire ici le code pour vérifier la présence du jeton csrf dans le contenu de la réponse HTTP  
-        c'est à dire affirmer que le contenu de la réponse contient 'type="hidden" name="_csrf_token"'
-        */
+        //$this->assertCount(1, $crawler->filter( '<input type="hidden" name="_csrf_token">'));
+        $this->assertContains(
+            '_csrf_token',
+            $this->client->getResponse()->getContent()
+        );
 
-        
         // Asserts that the response content contains 'input type="text" id="inputEmail"'
-        /*
-        Ecrire ici le code pour vérifier la présence du champ de formulaire email dans le contenu de la réponse HTTP  
-        c'est à dire affirmer que le contenu de la réponse contient '<input type="email" value="" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>'
-        */
+        $this->assertContains(
+            '<input type="text" value="" name="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>',
+            $this->client->getResponse()->getContent()
+        );
 
         
         // Asserts that the response content contains 'input type="text" id="inputPassword"'
-        /*
-        Ecrire ici le code pour vérifier la présence du champ de formulaire password dans le contenu de la réponse HTTP  
-        c'est à dire affirmer que le contenu de la réponse contient '<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>'
-        */
+        $this->assertContains(
+            '<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>',
+            $this->client->getResponse()->getContent()
+        );
         
     }
 }
