cd481429caf1d632857c12e79ab6a75d1814b996
MORTREUX Florian <mortflo@ndlp.fr>
2019-12-16 12:55:29 +0100

    [CORE] Debut QUESTION

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -32,6 +32,12 @@ class Answer
      */
     private $Correct;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Question", inversedBy="answers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $question;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -57,6 +63,18 @@ class Answer
     public function setCorrect(bool $Correct): self
     {
         $this->Correct = $Correct;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): self
+    {
+        $this->question = $question;
 
         return $this;
     }
