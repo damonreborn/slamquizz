e0380e143ac0d24be6804289dbd57e7c1d80451f
MORTREUX Florian <mortflo@ndlp.fr>
2019-12-16 09:40:27 +0100

    Correction Migration provoquer par Answer

--- src/Migrations/Version20191216083930.php
+++ src/Migrations/Version20191216083930.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191209101733 extends AbstractMigration
+final class Version20191216083930 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,6 +23,9 @@ final class Version20191209101733 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE TABLE tbl_answer (id INT NOT NULL, text VARCHAR(255) NOT NULL, correct BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE tbl_category (id INT NOT NULL, shortname VARCHAR(50) NOT NULL, longname VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE tbl_user (id INT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_38B383A1F85E0677 ON tbl_user (username)');
     }
 
     public function down(Schema $schema) : void
@@ -32,5 +35,7 @@ final class Version20191209101733 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE tbl_answer');
+        $this->addSql('DROP TABLE tbl_category');
+        $this->addSql('DROP TABLE tbl_user');
     }
 }
