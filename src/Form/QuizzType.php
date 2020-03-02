f61cbbc49b680e4e2514d7faa0d755368e55c641
Mortreux Florian <damonreborn@gmail.com>
2020-03-02 12:55:37 +0100

    Modification Categories non finis

---
+++ src/Form/QuizzType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Quizz;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Category;
+
+class QuizzType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('summary')
+            ->add('number_of_questions')
+            ->add('active')
+        ;
+
+        $builder->add('categories', EntityType::class, array(
+            'class' => Category::class,
+            'choice_label' => 'longname',
+            'multiple' => true
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Quizz::class,
+        ]);
+    }
+}
